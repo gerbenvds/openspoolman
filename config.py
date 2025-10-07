05e6e2454186ff9835dcb163a6e8a5fdca468ea5
Gerben van der Stouwe <gerben@vdstouwe.nl>
2025-10-07 23:51:29 +0200

    Allow configuring API_URL separately from BASE_URL

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ PRINTER_CODE = os.getenv('PRINTER_ACCESS_CODE')       # Printer access code - Ru
 PRINTER_IP = os.getenv('PRINTER_IP')     # Printer local IP address - Check wireless on printer
 PRINTER_NAME = os.getenv('PRINTER_NAME')     # Printer name - Check wireless on printer
 SPOOLMAN_BASE_URL = os.getenv('SPOOLMAN_BASE_URL')
-SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
+SPOOLMAN_API_URL = os.getenv('SPOOLMAN_API_URL', f"{SPOOLMAN_BASE_URL}/api/v1")
 AUTO_SPEND = os.getenv('AUTO_SPEND', False)
+
 SPOOL_SORTING = os.getenv('SPOOL_SORTING', "filament.material:asc,filament.vendor.name:asc,filament.name:asc")
